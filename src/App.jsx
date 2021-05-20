d8e6f2a4682e655b75234f70b5562ebe674e1995
Priyatham1431 <priyathamnayak@gmail.com>
2021-05-20 23:12:17 +0530

    basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './Homepage/Homepage';
 
 function App() {
   return (
-    <div className="">
-      <p>Hello World</p>
+    <div className="container">
+      <Homepage />
     </div>
   );
 }
